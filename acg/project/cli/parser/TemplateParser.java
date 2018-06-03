c132eda8c6224bd282dc30067584eb1480d66ce5
RichardTeller <32319142+RichardTeller@users.noreply.github.com>
2018-06-03 11:41:00 -0700

    Add files via upload

--- acg/project/cli/parser/TemplateParser.java
+++ acg/project/cli/parser/TemplateParser.java
@@ -5,16 +5,21 @@ import java.util.Scanner;
 import acg.project.action.ActionSet;
 import acg.project.action.command.A_Command;
 import acg.project.action.command.creational.define.*;
+import acg.architecture.datatype.*;
 
 
 public class TemplateParser extends Parser {
 
+    private String cmd;
+    
     public TemplateParser(ActionSet actionSet) {
         super(actionSet);
     }
     
     @Override
     public void parseCommand(String cmd) throws ParseException {
+        
+        this.cmd = cmd;
         
         Scanner cmdScanner = new Scanner(cmd);
         
@@ -26,7 +31,7 @@ public class TemplateParser extends Parser {
             
             token = cmdScanner.next();
         
-            if (token.equals("DEFINE")) {
+            if (token.equalsIgnoreCase("DEFINE")) {
                 
                 // DEFINE... commands
                 
@@ -34,39 +39,39 @@ public class TemplateParser extends Parser {
                    
                     token = cmdScanner.next();
                     
-                    if (token.equals("TRAP"))
+                    if (token.equalsIgnoreCase("TRAP"))
                         the_command = createCommand_DEFINE_TRAP();
                     
-                    else if (token.equals("CATAPULT"))
+                    else if (token.equalsIgnoreCase("CATAPULT"))
                         the_command = createCommand_DEFINE_CATAPULT();
                     
-                    else if (token.equals("OLS_XMT"))
+                    else if (token.equalsIgnoreCase("OLS_XMT"))
                         the_command = createCommand_DEFINE_OLS_XMT();
                     
-                    else if (token.equals("CARRIER"))
+                    else if (token.equalsIgnoreCase("CARRIER"))
                         the_command = createCommand_DEFINE_CARRIER();
                     
-                    else if (token.equals("FIGHTER"))
+                    else if (token.equalsIgnoreCase("FIGHTER"))
                         the_command = createCommand_DEFINE_FIGHTER();
                     
-                    else if (token.equals("TANKER"))
+                    else if (token.equalsIgnoreCase("TANKER"))
                         the_command = createCommand_DEFINE_TANKER();
                     
-                    else if (token.equals("BOOM")) {
+                    else if (token.equalsIgnoreCase("BOOM")) {
                         
                         if (cmdScanner.hasNext()) {
                             
                             token = cmdScanner.next();
                             
-                            if (token.equals("MALE"))
+                            if (token.equalsIgnoreCase("MALE"))
                                 the_command = createCommand_DEFINE_BOOM_MALE();
                             
-                            else if (token.equals("FEMALE"))
+                            else if (token.equalsIgnoreCase("FEMALE"))
                                 the_command = createCommand_DEFINE_BOOM_FEMALE();
                         }
                     }
                     
-                    else if (token.equals("BARRIER"))
+                    else if (token.equalsIgnoreCase("BARRIER"))
                         the_command = createCommand_DEFINE_BARRIER();
                 }
                 
@@ -75,7 +80,7 @@ public class TemplateParser extends Parser {
                 
             }
             
-            else if (token.equals("SHOW")) {
+            else if (token.equalsIgnoreCase("SHOW")) {
                 the_command = createCommand_SHOW_TEMPLATE();
                 
                 if (the_command != null)
@@ -89,13 +94,44 @@ public class TemplateParser extends Parser {
             throw new ParseException("Invalid command");
     }
 	
-    private CommandCreationalDefineTrap createCommand_DEFINE_TRAP() {
+    private CommandCreationalDefineTrap createCommand_DEFINE_TRAP() throws ParseException {
         
-        // process the command text and create the command object and return.
-        // if the command cannot be created due to invalid syntax, return null
-        // or throw an ParseException
+        // DEFINE TRAP <tid> ORIGIN <origin> AZIMUTH <azimuth> WIDTH <distance> LIMIT WEIGHT <weight> 
+        // SPEED <speed> MISS <percent>
         
-        return new CommandCreationalDefineTrap(null, null, null, null, null, null, null);
+        Scanner cmdScanner = new Scanner(this.cmd);
+        String token = "";
+        CommandCreationalDefineTrap TRAP;
+        
+        try {
+            cmdScanner.next(); // DEFINE
+            cmdScanner.next(); // TRAP
+        }
+        catch (Exception e) {
+            throw new ParseException();
+        }
+        
+        try {
+            
+            token = cmdScanner.next(); // <tid>
+            Identifier tid = ParseUtils.parseID(token);
+            
+            cmdScanner.next(); // ORIGIN
+            
+            token = cmdScanner.next(); // <origin>
+            CoordinateCartesianRelative origin = ParseUtils.parseORIGIN(token);
+            
+            cmdScanner.next(); // AZIMUTH
+            
+            token = cmdScanner.next(); // <azimuth>
+            // AngleNavigational azimuth = ParseUtils.parseAZIMUTH(token);
+            
+            
+            return new CommandCreationalDefineTrap (tid, origin, null, null, null, null, null);
+        }
+        catch (Exception e){
+            throw new ParseException();
+        }
     }
 	
     private A_Command createCommand_SHOW_TEMPLATE() {
