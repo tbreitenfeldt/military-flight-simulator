ebe4f65e01b9a9d41fd8d46320291b9a4d68cdf4
TJ Breitenfeldt <timothyjb310@gmail.com>
2018-06-03 20:14:52 -0700

    aded parseCOORDINATES to ParseUtils, and aded createCarrier to ParseAgent

--- acg/project/cli/parser/AgentParser.java
+++ acg/project/cli/parser/AgentParser.java
@@ -1,6 +1,7 @@
 package acg.project.cli.parser;
 
 import java.util.Scanner;
+import java.util.NoSuchElementException;
 
 import acg.project.action.ActionSet;
 import acg.project.action.command.A_Command;
@@ -20,35 +21,36 @@ public class AgentParser extends Parser {
         String token = "";
         A_CommandCreationalCreate theCommand = null;
         
-        cmdScanner.useDelimiter(" ");
-        
         if ( !cmdScanner.hasNext()) {
             throw new ParseException("Command given is empty");
         }//end if
         
-        token = cmdScanner.next();
+        token = cmdScanner.next();  //create
         
-        if ( !token.equalsIgnoreCase("create") || !cmdScanner.hasNext()) {
+        if ( !token.equalsIgnoreCase("create")) {
+            throw new ParseException("Invalid command");
+        }//end if
+        if (!cmdScanner.hasNext()) {
             throw new ParseException("Invalid command");
         }//end if
         
-        token = cmdScanner.next();
+        token = cmdScanner.next();  //agent
         
         //parse agents 
         if (token.equalsIgnoreCase("carrier")) {
-            theCommand = createCommandCarrier(cmdScanner);
+            theCommand = createCommandCarrier(cmd);
             this.actionSet.getActionCreationalCreate().submit((CommandCreationalCreateCarrier) theCommand);   
         } else if (token.equalsIgnoreCase("tanker")) {
-            theCommand = createCommandTanker(cmdScanner);
+            theCommand = createCommandTanker(cmd);
             this.actionSet.getActionCreationalCreate().submit((CommandCreationalCreateTanker) theCommand);
         } else if (token.equalsIgnoreCase("ols_xmt")) {
-            theCommand = createCommandOLS_XMT(cmdScanner);
+            theCommand = createCommandOLS_XMT(cmd);
             this.actionSet.getActionCreationalCreate().submit((CommandCreationalCreateOLSTransmitter) theCommand);
         } else if (token.equalsIgnoreCase("tailhook")) {
-            theCommand = createCommandTailhook(cmdScanner);
+            theCommand = createCommandTailhook(cmd);
             this.actionSet.getActionCreationalCreate().submit((CommandCreationalCreateTailhook) theCommand);
         } else if (token.equalsIgnoreCase("fighter")) {
-            theCommand = createCommandFighter(cmdScanner);
+            theCommand = createCommandFighter(cmd);
             this.actionSet.getActionCreationalCreate().submit((CommandCreationalCreateFighter) theCommand);
         } else {
             throw new ParseException("Invalid command");
@@ -57,7 +59,7 @@ public class AgentParser extends Parser {
         cmdScanner.close();
     }//end method
     
-    private CommandCreationalCreateCarrier createCommandCarrier(Scanner cmdScanner) throws ParseException {
+    private CommandCreationalCreateCarrier createCommandCarrier(String cmd) throws ParseException {
         Identifier idAgentCarrier = null;
         Identifier idTemplateCarrier = null;
         Identifier idAgentCatapult = null;
@@ -68,84 +70,114 @@ public class AgentParser extends Parser {
         AngleNavigational heading = null;
         Speed speed = null;
         String token = "";
+        Scanner cmdScanner = new Scanner(cmd);
         
-        if ( !cmdScanner.hasNext()) {
-            throw new ParseException("Invalid command, create carrier must have a agent id");
-        }//end if
+        try {
+            token = cmdScanner.next(); //create 
+            token += " " + cmdScanner.next();  //carrier
+            
+            if (token.equalsIgnoreCase("create carrier")) {
+                throw new ParseException("Invalid create carrier command");
+            }//end if
+            
+            token = cmdScanner.next();  //carrier agent ID
+            idAgentCarrier = ParseUtils.parseID(token);
+            
+            if ( !cmdScanner.next().equalsIgnoreCase("from")) {
+                throw new ParseException("Invalid create carrier command, expects \"from\"");
+            }//end if
+            
+            token = cmdScanner .next();  //carrier template id
+            idTemplateCarrier = ParseUtils.parseID(token);
+            
+            if ( !cmdScanner.next().equalsIgnoreCase("with")) {
+                throw new ParseException("Invalid create carrier command, expects \"with\"");
+            }//end if
+            
+            if ( !cmdScanner.next().equalsIgnoreCase("catapult")) {
+                throw new ParseException("Invalid create carrier command, expects \"catapult\"");
+            }//end if
+            
+            token = cmdScanner.next();  //catapult agent id
+            idAgentCatapult = ParseUtils.parseID(token);
+            
+            if ( !cmdScanner.next().equalsIgnoreCase("barrier")) {
+                throw new ParseException("Invalid create carrier command, expects \"barrier\"");
+            }//end if
+            
+            token = cmdScanner.next();  //barrier agent id
+            idAgentBarrier = ParseUtils.parseID(token);
+            
+            if ( !cmdScanner.next().equalsIgnoreCase("trap")) {
+                throw new ParseException("Invalid create carrier command, expects \"trap\"");
+            }//end if
+            
+            token = cmdScanner.next();  //trap agent id
+            idAgentTrap = ParseUtils.parseID(token);
+            
+            token = cmdScanner.next();  //optical-landing-system or ols 
+            if (token.equalsIgnoreCase("ols")) {
+                idAgentOLS = ParseUtils.parseID(token);
+            }else {
+                
+                if (token.equalsIgnoreCase("optical-landing-system") && cmdScanner.next().equalsIgnoreCase("transmitter")) {
+                    token = cmdScanner.next();  //ols agent id
+                    idAgentOLS = ParseUtils.parseID(token);
+                } else {
+                    throw new ParseException("Invalid create carrier command, expects \"ols\" or \"optical-landing-system transmitter\"");
+                }//end else
+            }//end end else
+            
+            if ( !cmdScanner.next().equalsIgnoreCase("at")) {
+                throw new ParseException("Invalid create carrier command, expects \"at\"");
+            }//end if
+            if ( !cmdScanner.next().equalsIgnoreCase("coordinates")) {
+                throw new ParseException("Invalid create carrier command, expects \"at\"");
+            }//end if
+            
+            token = cmdScanner.next();  //coordinates in laditude/longitude
+            coordinates = ParseUtils.parseCOORDINATES(token);
+            
+            if ( !cmdScanner.next().equalsIgnoreCase("heading")) {
+                throw new ParseException("Invalid create carrier command, expects \"heading\"");
+            }//end if
+            
+            token = cmdScanner.next();
+            heading = ParseUtils.parseAZIMUTH(token);
+            
+            if ( !token.equalsIgnoreCase("speed")) {
+                throw new ParseException("Invalid create carrier command, expects \"speed\"");
+            }//end if 
+            
+            token = cmdScanner.next();
+            speed = ParseUtils.parseSPEED(token);
+            
+            if (cmdScanner.hasNext()) {
+                throw new ParseException("Correct command, but accessive sintax");
+            }//end if
+        } catch(ParseException pe) {
+            throw pe;
+        } catch(NoSuchElementException nsee) {  //catch exceptions if there are no characters left in the scanner
+            throw new ParseException("Incomplete command.");
+        }//end catch
         
-        idAgentCarrier = new Identifier(cmdScanner.next());
-        
-        if ( !cmdScanner.next().equalsIgnoreCase("from")) {
-            throw new ParseException("Invalid create agent command, expects \"from\"");
-        }//end if
-        if ( !cmdScanner.hasNext()) {
-            throw new ParseException("Invalid command, expects carrier tid");
-        }//end if
-        
-        idTemplateCarrier = new Idetifier(cmdScanner.next();
-        
-        if ( !cmdScanner.next().equalsIgnoreCase("with")) {
-            throw new ParseException("Invalid create agent command, expects \"with\"");
-        }//end if
-        
-        if ( !cmdScanner.next().equalsIgnoreCase("catapult")) {
-            throw new ParseException("Invalid create agent command, expects \"catapult\"");
-        }//end if
-        if ( !cmdScanner.hasNext()) {
-            throw new ParseException("Invalid command, expects catapult's agent id");
-        }//end if
-        
-        idAgentCatapult = new Identifier(cmdScanner.next());
-        
-        if ( !cmdScanner.next().equalsIgnoreCase("barrier")) {
-            throw new ParseException("Invalid create agent command, expects \"barrier\"");
-        }//end if
-        if ( !cmdScanner.hasNext()) {
-            throw new ParseException("Invalid command, expects barrier's agent id");
-        }//end if
-        
-        idAgentBarrier = new Identifier(cmdScanner.next());
-        
-        if ( !cmdScanner.next().equalsIgnoreCase("trap")) {
-            throw new ParseException("Invalid create agent command, expects \"trap\"");
-        }//end if
-        if ( !cmdScanner.hasNext()) {
-            throw new ParseException("Invalid command, expects trap's agent id");
-        }//end if
-        
-        idAgentTrap = new Identifier(cmdScanner.next());
-        token = cmdScanner.next();
-        
-        if (token.equalsIgnoreCase("ols") &&cmdScanner.hasNext()) {
-            idAgentOLS = new Identifier(cmdScanner.next());
-        }else {
-
-            if (token.equalsIgnoreCase("optical-landing-system") && cmdScanner.hasNext() && cmdScanner.next().equalsIgnoreCase("transmitter")) {
-                idAgentOLS = new Identifier(cmdScanner.next());
-            } else {
-                throw new ParseException("Invalid create agent command, expects \"ols\" or \"optical-landing-system transmitter\"");
-            }//end else
-        }//end end else
-        
-        //account for coordinates, heading 
-        
-        
+        cmdScanner.close();
         return new CommandCreationalCreateCarrier(idAgentCarrier, idTemplateCarrier, idAgentCatapult, idAgentBarrier, idAgentTrap, idAgentOLS, coordinates, heading, speed);
     }//end method
     
-    private CommandCreationalCreateTailhook createCommandTailhook(Scanner cmdScanner) throws ParseException {
+    private CommandCreationalCreateTailhook createCommandTailhook(String cmd) throws ParseException {
         return null;
     }//end method
 
-    private CommandCreationalCreateOLSTransmitter createCommandOLS_XMT(Scanner cmdScanner) throws ParseException {
+    private CommandCreationalCreateOLSTransmitter createCommandOLS_XMT(String cmd) throws ParseException {
         return null;
     }//end method
 
-    private CommandCreationalCreateTanker createCommandTanker(Scanner cmdScanner) throws ParseException {
+    private CommandCreationalCreateTanker createCommandTanker(String cmd) throws ParseException {
         return null;
     }//end method
 
-    private CommandCreationalCreateFighter createCommandFighter(Scanner cmdScanner) throws ParseException {
+    private CommandCreationalCreateFighter createCommandFighter(String cmd) throws ParseException {
         return null;
     }//end method
 
--- acg/project/cli/parser/ParseUtils.java
+++ acg/project/cli/parser/ParseUtils.java
@@ -1,10 +1,14 @@
 package acg.project.cli.parser;
 
 import java.util.Scanner;
+import java.util.NoSuchElementException;
+import java.util.InputMismatchException;
+
 import acg.architecture.datatype.*;
 
+
 public class ParseUtils {
-    
+
     public static Identifier parseID(String token) throws ParseException {
         
         if (token.length() == 0)
@@ -16,73 +20,139 @@ public class ParseUtils {
         else
             throw new ParseException();
         
-    }
+    }//end method
     
     public static CoordinateCartesianRelative parseORIGIN(String token) {
-    	
-    	String tokenLeft = token.substring(0, token.indexOf(":"));
-    	String tokenRight = token.substring(token.indexOf(":")+1, token.length());
-    	
-    	Double doubleLeft = Double.parseDouble(tokenLeft);
-    	Double doubleRight = Double.parseDouble(tokenRight);
-    	
-    	return new CoordinateCartesianRelative(doubleLeft, doubleRight);
-    }
+        
+        String tokenLeft = token.substring(0, token.indexOf(":"));
+        String tokenRight = token.substring(token.indexOf(":")+1, token.length());
+        
+        Double doubleLeft = Double.parseDouble(tokenLeft);
+        Double doubleRight = Double.parseDouble(tokenRight);
+        
+        return new CoordinateCartesianRelative(doubleLeft, doubleRight);
+    }//end method
     
     public static AngleNavigational parseAZIMUTH(String token) throws ParseException {
-    	
-    	if (token.contains("-") || token.contains("+"))
-    		throw new ParseException();
-    	
-    	Integer angle = Integer.parseInt(token);
-    	
-    	if (angle >= 0 && angle < 360)
-    		return new AngleNavigational(angle);
-    	
-    	throw new ParseException();
-    }
-
+        
+        if (token.contains("-") || token.contains("+"))
+            throw new ParseException();
+        
+        Integer angle = Integer.parseInt(token);
+        
+        if (angle >= 0 && angle < 360)
+            return new AngleNavigational(angle);
+        
+        throw new ParseException();
+    }//end method
+    
     public static Distance parseDISTANCE(String token) throws ParseException {
-    	
-    	if (token.contains("-") || token.contains("+"))
-    		throw new ParseException();
-    	
-    	Double distance = Double.parseDouble(token);
-    	
-    	return new Distance(distance);
-    }
+        
+        if (token.contains("-") || token.contains("+"))
+            throw new ParseException();
+        
+        Double distance = Double.parseDouble(token);
+        
+        return new Distance(distance);
+    }//end method
     
     public static Weight parseWEIGHT(String token) throws ParseException {
-    	
-    	if (token.contains("-") || token.contains("+"))
-    		throw new ParseException();
-    	
-    	Integer weight = Integer.parseInt(token);
-    	
-    	return new Weight(weight);
-    }
+        
+        if (token.contains("-") || token.contains("+"))
+            throw new ParseException();
+        
+        Integer weight = Integer.parseInt(token);
+        
+        return new Weight(weight);
+    }//end method
     
     public static Speed parseSPEED(String token) throws ParseException {
-    	
-    	if (token.contains("-") || token.contains("+"))
-    		throw new ParseException();
-    	
-    	Integer speed = Integer.parseInt(token);
-    	
-    	return new Speed(speed);
-    }
+        
+        if (token.contains("-") || token.contains("+"))
+            throw new ParseException();
+        
+        Integer speed = Integer.parseInt(token);
+        
+        return new Speed(speed);
+    }//end method
     
     public static Percent parsePERCENT(String token) throws ParseException {
-    	
-    	if (token.contains("-") || token.contains("+"))
-    		throw new ParseException();
-    	
-    	Integer percent = Integer.parseInt(token);
-    	
-    	if (percent < 0 || percent > 100)
-    		throw new ParseException();
-    	
-    	return new Percent(percent);
-    }
+        
+        if (token.contains("-") || token.contains("+"))
+            throw new ParseException();
+        
+        Integer percent = Integer.parseInt(token);
+        
+        if (percent < 0 || percent > 100)
+            throw new ParseException();
+        
+        return new Percent(percent);
+    }//end method
     
-}
+    public static CoordinateWorld parseCOORDINATES(String token) throws ParseException {
+        Latitude laditude = null;
+        Longitude longitude = null;
+        int degrees = 0;
+        int minutes = 0;
+        double seconds = 0.0;
+        Scanner coordinateScanner = null;
+        
+        //Insure that the token is in the format int*int'decimal(with or with out .)"/int*int'decimal"
+        if ( !token.matches("\\d+\\*\\d+\'\\d+\\.{0,1}\\d+\"/\\d+\\*\\d+\'\\d+\\.{0,1}\\d+\"")) {
+            throw new ParseException("Coordinates have invalid format");
+        }//end if
+        
+        coordinateScanner = new Scanner(token);
+        coordinateScanner.useDelimiter("[*\'\"/]");
+        
+        try {
+            degrees = coordinateScanner.nextInt();
+            
+            if (degrees < 0 || degrees > 90) {
+                throw new ParseException("Laditude degrees must be between 0 and 90 inclusive");
+            }//end if
+            
+            minutes = coordinateScanner.nextInt();
+            
+            if (minutes < 0 || minutes > 59) {
+                throw new ParseException("Laditude minutes must be in the range 0 to 59 inclusive");
+            }//end if
+            
+            seconds = coordinateScanner.nextDouble();
+            
+            if (seconds < 0 || seconds > 59) {
+                throw new ParseException("Laditude seconds must be in the range 0 to 59 inclusive");
+            }//end if
+            
+            laditude = new Latitude(degrees, minutes, seconds);
+            coordinateScanner.next();  //eat up blank space seperating laditude and longitude 
+            
+            degrees = coordinateScanner.nextInt();
+            
+            if (degrees < 0 || degrees > 180) {
+                throw new ParseException("Longitude degrees must be between 0 and 180 inclusive");
+            }//end if
+            
+            minutes = coordinateScanner.nextInt();
+            
+            if (minutes < 0 || minutes > 59) {
+                throw new ParseException("Longitude minutes must be in the range 0 to 59 inclusive");
+            }//end if
+        
+            seconds = coordinateScanner.nextDouble();
+            
+            if (seconds < 0 || seconds > 59) {
+                throw new ParseException("Longitude seconds must be in the range 0 to 59 inclusive");
+            }//end if
+            
+            longitude = new Longitude(degrees, minutes, seconds);
+        } catch(InputMismatchException ime) {  //catch exceptions for if nextInt or nextDouble are unable to parse correctly 
+            throw new ParseException("invalid format for coordinates");
+        } catch(NoSuchElementException nsee) {  //catch exceptions if there are no characters left in the scanner
+            throw new ParseException("Invalid coordinates");
+        }//end catch
+        
+        return new CoordinateWorld(laditude, longitude);
+    }//end method
+
+}//end class
