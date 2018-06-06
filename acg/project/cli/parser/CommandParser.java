b3a7529446e6cc4dbb6caddeda9fbceb8adb291a
RichardTeller <32319142+RichardTeller@users.noreply.github.com>
2018-06-06 12:48:23 -0700

    Add files via upload

--- acg/project/cli/parser/CommandParser.java
+++ acg/project/cli/parser/CommandParser.java
@@ -1,17 +1,22 @@
 
 package acg.project.cli.parser;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Scanner;
 
 import acg.project.action.ActionSet;
+
+// Richard: Template
+// TJ: Agent, Misc
+// Coel: Structural, Behavioral
 
 
 public class CommandParser {
     
     private String cmd;
     private Hashtable<String, Parser> parsers;
-    private ActionSet  actionSet;
+    private ActionSet actionSet;
 
 	public CommandParser(acg.project.action.ActionSet actionSet, String cmd) {
         this.cmd = cmd;
@@ -31,32 +36,49 @@ public class CommandParser {
     }
 
 	public void interpret() throws ParseException {
-        Scanner cmdScanner = new Scanner(cmd);
-        String firstWord = "";
-        Parser parser = null;
-        
-        if (cmdScanner.hasNext()) {
-            
-            firstWord = cmdScanner.next();
-            firstWord = firstWord.toUpperCase();
-            cmdScanner.close();
-            cmdScanner = null;
-            
-            try {
-                parser = parsers.get(firstWord); // manually catch the hashtable throwing an exception if the command doesnt exist and
-                // throw it ourselves because it is an invalid command
-            }
-            catch (Exception e) {
-                throw new ParseException("Invalid command");
-            }
-            
-            if (parser != null)
-                parser.parseCommand(this.cmd);
-        }
-        
-        if (cmdScanner != null) {
-            cmdScanner.close();
-            cmdScanner = null;
-        }
+		
+		ArrayList<String> commands = new ArrayList<String>();
+		loadCommands(commands, this.cmd);
+		
+		for (String command: commands) {
+			
+	        Scanner cmdScanner = new Scanner(command);
+	        String firstWord = "";
+	        Parser parser = null;
+	        
+	        if (cmdScanner.hasNext()) {
+	            
+	            firstWord = cmdScanner.next();
+	            firstWord = firstWord.toUpperCase();
+	            cmdScanner.close();
+	            cmdScanner = null;
+	            
+	            try {
+	                parser = parsers.get(firstWord); // manually catch the hashtable throwing an exception if the command doesnt exist and
+	                // throw it ourselves because it is an invalid command
+	            }
+	            catch (Exception e) {
+	                throw new ParseException("Invalid command");
+	            }
+	            
+	            if (parser != null)
+	                parser.parseCommand(this.cmd);
+	            
+	        }
+	        
+	        if (cmdScanner != null) {
+	            cmdScanner.close();
+	            cmdScanner = null;
+	        }
+		}
+	}
+	
+	private static void loadCommands(ArrayList<String> commands, String commandText) {
+		/*
+		 * Take the commands which may be more than one from the commandText string, and seperate
+		 * them into individual commands and load those into the ArrayList commands so that each
+		 * entry in the commands ArrayList is a single complete command, no comments.
+		 */
+		
 	}
 }
